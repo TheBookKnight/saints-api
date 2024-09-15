5b7dfe4123781934470744def24367924062cc62
TheBookKnight <cadav001@umn.edu>
2024-09-15 17:49:11 -0400

    3 - Expose GET saint by ID route

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import dotenv from "dotenv";
-dotenv.config();
+import saints from "./server/routes/saints.js"
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Saints API service listening on port ${port}`)
-})
+});
+
+// Load the /saints routes
+app.use("/saints", saints);
